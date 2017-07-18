c9ea2e9c5979fb3a6b1b6783f68db225629df91f
James Le <le_j6@denison.edu>
2017-07-18 11:21:47 -0700

    Build out Task Component

---
+++ client/components/Task.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { putChangeStatus, deleteTask } from '../redux/reducers/reducer';
+
+const Task = (props) => {
+  return (
+      <div className="row">
+        <div className="btn-group" role="group" aria-label="Basic example">
+          <button type="button" onClick={() => {
+            props.putChangeStatus(props.Obj, props.isComplete)}} className="btn">{props.isComplete ? "Undo" : "Complete" }</button>
+          <button type="button" onClick={() => props.deleteTask(props.Obj.slug)} className="btn">Delete</button>
+        </div>
+        <h3 style={{textDecoration: props.isComplete ? "line-through" : "none"}}>{props.Name}</h3>
+      </div>
+  );
+};
+
+const mapDispatch = {putChangeStatus, deleteTask};
+export default connect(null, mapDispatch)(Task);
